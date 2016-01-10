ad864356fcea571ffc364bcb29ef57e772d2cbf8
IndieDriver <amathias63@gmail.com>
2016-01-10 16:47:12 +0100

    fdf: HS mlx_image to implement

--- fdf/main.c
+++ fdf/main.c
@@ -6,61 +6,60 @@
 /*   By: amathias <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/20 15:17:52 by amathias          #+#    #+#             */
-/*   Updated: 2016/01/09 16:10:50 by amathias         ###   ########.fr       */
+/*   Updated: 2016/01/10 16:41:44 by amathias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-/*
-void	draw(void *mlx, void *win)
+int		expose_hook(t_map *map)
 {
-	int x;
-	int y;
-	t_point point;
-
-	y = 50;
-	while (y < 300)
-	{
-		x = 50;
-		while (x < 300)
-		{
-			point = convertcord(x, y);
-			mlx_pixel_put(mlx, win, point.x, point.y, 0xFF0000);
-			x += 10;
-		}
-		y++;
-	}
-	x = 50;
-	while (x < 300)
-	{
-		y = 50;
-		while (y < 300)
-		{
-			point = convertcord(x, y);
-			mlx_pixel_put(mlx, win, point.x, point.y, 0x0033FF);
-			y += 10;
-		}
-		x++;
-	}
-}
-*/
-
-int		expose_hook(t_env *e)
-{
-	int grid[4][4] = { {0,0,0,0} , {0,42,42,0} , {0,42,42,0} , {0,0,0,0} };
-	t_point **pointgrid = init_grid(grid, 4, 4, 50);
-	draw_iso(*e, pointgrid, 4, 4);
+	map->env.img = mlx_new_image(map->env.mlx, 640, 480);
+	draw_iso(map->env, map->map, map->height, map->width);
 	return (0);
 }
 
+void	ft_error(void)
+{
+	ft_putstr("error\n");
+	exit(0);
+}
+
+void 	ft_setup(t_map *map)
+{
+	map->width = 19;
+	map->height = 11;
+}
 int		main(int argc, char **argv)
 {
 	t_env e;
-
+	//int fd;
+	t_map *map;
+	int grid[11][19] = { {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+						 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+						 {0,0,10,10,0,0,10,10,0,0,0,0,0,0,0,0,0,0,0},
+						 {0,0,10,10,0,0,10,10,0,0,0,0,0,0,0,0,0,0,0},
+						 {0,0,10,10,0,0,10,10,0,0,0,0,0,0,0,0,0,0,0},
+						 {0,0,10,10,10,10,10,10,0,0,0,0,0,0,0,0,0,0,0},
+						 {0,0,0,10,10,10,10,10,0,0,0,0,0,0,0,0,0,0,0},
+						 {0,0,0,0,0,0,10,10,0,0,0,0,0,0,0,0,0,0,0},
+						 {0,0,0,0,0,0,10,10,0,0,0,0,0,0,0,0,0,0,0},
+						 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+						 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0} };
+	map = malloc(sizeof(t_map));
+	map->map = init_grid(grid,11,19,15);
 	e.mlx = mlx_init();
 	e.win = mlx_new_window(e.mlx, WIDTH, HEIGHT, "42");
-	mlx_expose_hook(e.win, expose_hook, &e);
+	if (argc != 2)
+		ft_error();
+	else
+	{
+		ft_setup(map);
+		//fd = open(argv[1], O_RDONLY);
+		//map = ft_parse(fd, map);
+		map->env = e;
+		mlx_expose_hook(e.win, expose_hook, map);
+	}
 	mlx_loop(e.mlx);
 	sleep(5);
 	if (argc == 1)
