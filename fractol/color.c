d802af6cd100349628b00d24a518c73d64bad001
IndieDriver <amathias63@gmail.com>
2016-01-25 10:28:19 +0100

    fractol: added color.c and hook.c

---
+++ fractol/color.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amathias <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/24 14:13:19 by amathias          #+#    #+#             */
+/*   Updated: 2016/01/24 15:37:08 by amathias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int		*init_mandelbrot_color(int max_iter)
+{
+	int *color_array;
+	int i;
+	int tmp;
+
+	tmp = 0xff6600;
+	if ((color_array = (int *)malloc(sizeof(int) * max_iter + 1)) == NULL)
+		return (NULL);
+	i = 0;
+	printf("%#08x\n", 0xff6600 - 0xff1a75);
+	printf("%#08x\n", 0xe65c00 - 0xff6600);
+	while (i < max_iter + 1)
+	{
+		tmp += 0x000301;
+		printf("%#08x\n",tmp);
+		if (i == max_iter)
+			color_array[i] = 0x000000;
+		else
+			color_array[i] = tmp;
+		i++;
+	}
+	return (color_array);	
+}
---
+++ fractol/hook.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hook.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amathias <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/24 10:30:38 by amathias          #+#    #+#             */
+/*   Updated: 2016/01/24 14:50:33 by amathias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int		expose_hook(t_map *map)
+{
+	draw(map);
+	return (0);
+}
+
+int		motion_notify(int x, int y, t_map *map)
+{
+	if (x > 0 && y > 0 && x < WIDTH && y < WIDTH && map->type == 1)
+	{
+		map->c.real = 1.5 * (x - (WIDTH / 2)) / (WIDTH * map->zoom / 2);
+		map->c.ima = 1.5 * (y - (HEIGHT / 2)) / (HEIGHT * map->zoom / 2);
+		draw(map);
+	}
+	return (0);
+}
+
+int		key_hook(int keycode, t_map *map)
+{
+	if (keycode == 24)
+	{
+		map->pow++;
+		map->zoom *= pow(1.001, map->pow);
+		draw(map);
+	}
+	if (keycode == 27)
+	{
+		map->pow = map->pow < 1 ? 1 : map->pow - 1;
+		map->zoom = map->zoom < 1 ? 1 : map->zoom / pow(1.001, map->pow);
+		draw(map);
+	}
+	if (keycode == 53)
+	{
+		mlx_destroy_window(map->env.mlx, map->env.win);
+		exit(0);
+	}
+	return (0);
+}
