c73d6fb5d811912e1c9efb6c1273f3150972974f
IndieDriver <amathias63@gmail.com>
2016-02-01 12:00:39 +0100

    fdf: hidden tiles support (partially)

--- fdf/zbuffer.c
+++ fdf/zbuffer.c
@@ -6,65 +6,75 @@
 /*   By: amathias <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/11 14:46:57 by amathias          #+#    #+#             */
-/*   Updated: 2016/01/31 16:34:50 by amathias         ###   ########.fr       */
+/*   Updated: 2016/02/01 11:59:49 by amathias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+void	replace_color(t_map *map, int color)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < HEIGHT)
+	{
+		j = 0;
+		while (j < WIDTH)
+		{
+			if (get_hex_color(map, j, i) == 0xFFFFFF)
+				draw_pixel_to_image(map, j, i, color);
+			j++;
+		}
+		i++;
+	}
+}
 
 void	fill_polygon(t_map *map)
 {
 	t_point point;
 
 	point = get_point(map);
-	//if (point.x != 0)
 	fill_rect(map, point.x, point.y, 0xFFFFFF);
+	replace_color(map, 0x000000);
+	printf("fill_polygon\n");
 }
 
-void	replace_color(t_map *map, color;)
-{
-	int x;
-	int y;
-
-	y = 0;
-	while (y < HEIGHT)
-	{
-		x = 0;
-		while (x < WIDTH)
-		{
-			//replace color 
-			x++;
-		}
-		y++:
-	}
-}
 t_point	get_point(t_map *map)
 {
 	int y;
 	int x;
-	int line;
+	int l;
+	int first;
 	t_point point;
 
-	line = 0;
+	l = 0;
 	y = 0;
+	first = 0;
 	while (y < HEIGHT)
 	{
 		x = 0;
-		line = 0;
+		l = 0;
 		point.x = -1;
 		while (x < WIDTH)
 		{
-			if (get_hex_color(map, x, y) == 0x0000FF && line == 0 && point.x == -1)
-				line++;
+			if (get_hex_color(map, x, y) == 0x0000FF && first < 10)
+			{
+				first++;
+				break ;
+			}
+			if (get_hex_color(map, x, y) == 0x0000FF && l == 0 && point.x == -1)
+				l++;
 			if (get_hex_color(map, x, y) == 0x0000FF && point.x != -1)
-				line++;
-			if (line == 1 && get_hex_color(map, x, y) != 0x0000FF
+				l++;
+			if (l == 1 && get_hex_color(map, x, y) != 0x0000FF
 					&& point.x == -1)
 			{
 				point.x = x;
 				point.y = y;
 			}
-			if (line == 2 && point.x != -1)
+			if (l == 2 && point.x != -1)
 			{
 				draw_pixel_to_image(map, point.x, point.y, 0xFFFF00);
 				return (point);
