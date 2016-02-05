d8041b20e553d3f90a6a1f2722f4d0a65e39951a
IndieDriver <amathias63@gmail.com>
2016-02-05 14:23:17 +0100

    wolf3d: colide detection

--- wolf3d/move.c
+++ wolf3d/move.c
@@ -6,12 +6,22 @@
 /*   By: amathias <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/05 11:28:56 by amathias          #+#    #+#             */
-/*   Updated: 2016/02/05 11:53:31 by amathias         ###   ########.fr       */
+/*   Updated: 2016/02/05 14:22:47 by amathias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
+int		colide(t_map *map, int signe, int lat)
+{
+	if (!lat && map->grid[(int)(map->pos.x + (signe * map->dirvec.x) * 0.1)]
+		[(int)(map->pos.y + (signe * map->dirvec.y) * 0.1)] != 0)
+		return (1);
+	if (lat && map->grid[(int)(map->pos.x - (signe * map->dirvec.y) * 0.1)]
+			[(int)(map->pos.y + (signe * map->dirvec.x) * 0.1)] != 0)
+		return (1);
+	return (0);
+}
 void	rotate(t_map *map)
 {
 	t_vec tmp;
@@ -49,25 +59,25 @@ void	rotate(t_map *map)
 void	move(t_map *map)
 {
 
-	if (map->key.up == 1)
+	if (map->key.up == 1 && !colide(map, 1, 0))
 	{
 		map->pos.x += map->dirvec.x * 0.1;
 		map->pos.y += map->dirvec.y * 0.1;
 		draw(map);
 	}
-	if (map->key.down == 1)
+	if (map->key.down == 1 && !colide(map,-1, 0))
 	{	
 		map->pos.x -= map->dirvec.x * 0.1;
 		map->pos.y -= map->dirvec.y * 0.1;
 		draw(map);
 	}
-	if (map->key.right == 1)
+	if (map->key.right == 1 && !colide(map, 1, 1))
 	{
 		map->pos.x += -map->dirvec.y * 0.1;
 		map->pos.y += map->dirvec.x * 0.1;
 		draw(map);
 	}
-	if (map->key.left == 1)
+	if (map->key.left == 1 && !colide(map, -1, 1))
 	{	
 		map->pos.x -= -map->dirvec.y * 0.1;
 		map->pos.y -= map->dirvec.x * 0.1;
