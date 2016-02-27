9692580d90105916b5b49c145812791e712d36ed
IndieDriver <amathias63@gmail.com>
2016-02-27 09:51:10 +0100

    rtv1:

--- RTv1/raytracer.c
+++ RTv1/raytracer.c
@@ -25,7 +25,6 @@ void	normalize_vec(t_vec *vec)
 double	get_contact(t_map *map, t_vec ray)
 {
 	t_sphere s;
-	t_vec inter;
 	double a;
 	double b;
 	double c;
@@ -33,14 +32,14 @@ double	get_contact(t_map *map, t_vec ray)
 
 	s = map->scene.sphere[0];
 	a = ray.x * ray.x + ray.y * ray.y + ray.z * ray.z;
-	b = 2 * ray.x * (map->scene.pos.x - s.pos.x) + 2 * ray.y *
-		(map->scene.pos.y - s.pos.y) + 2 * ray.z *
-		(map->scene.pos.z - s.pos.z);
-	c = (s.pos.x * s.pos.x + s.pos.y * s.pos.y + s.pos.z * s.pos.z)
-		+ (ray.x * ray.x + ray.y * ray.y + ray.z + ray.z)
-		- 2 * (ray.x * s.pos.x + ray.y * s.pos.y + ray.z * s.pos.z)
+	b = 2 * (ray.x * (map->scene.pos.x - s.pos.x)
+		+ ray.y * (map->scene.pos.y - s.pos.y)
+		+ ray.z * (map->scene.pos.z - s.pos.z));
+	c = (map->scene.pos.x - s.pos.x) * (map->scene.pos.x - s.pos.x)
+		+ (map->scene.pos.y - s.pos.y) * (map->scene.pos.y - s.pos.y)
+		+ (map->scene.pos.z - s.pos.z) * (map->scene.pos.z - s.pos.z)
 		- s.radius * s.radius;
-	d = b * b + 4 * a * c;
+	d = (b * b) - (4 * a * c);
 
 		/*inter.x = (rayvec.x * rayvec.x) + (rayvec.y * rayvec.y) + (rayvec.z * rayvec.z);
 		  inter.y = 2 * (rayvec.x * (map->scene.pos.x - 0.0) +
@@ -50,8 +49,8 @@ double	get_contact(t_map *map, t_vec ray)
 		  (map->scene.pos.y - 0.0) * (map->scene.pos.y - 0.0) +
 		  (map->scene.pos.z - 0.0) * (map->scene.pos.z - 0.0)) - (125.0) * (125.0);
 		  det = (inter.y * inter.y) - 4.0 * inter.x * inter.z; */
-		if (d > 0.0)
-			printf("det: %f\n", inter.y);
+		//if (d > 0.0)
+	printf("det: %f\n", d);
 	return (d);
 }
 
@@ -61,11 +60,11 @@ void	ray(t_map *map, int x, int y)
 {
 	double	tmp;
 
-	map->scene.rayvec.x = (double)(x - (map->scene.w / 2.0));
-	map->scene.rayvec.y = (double)(y - (map->scene.h / 2.0));
-	map->scene.rayvec.z = -(map->scene.w / 2 * tan((30.0 / 2.0) * M_PI / 180.0));
-	normalize_vec(&(map->scene.rayvec));
-	tmp = get_contact(map, map->scene.raydir, map->scene.rayvec);
+	map->scene.rayvec.x = (double)(x - (map->scene.w / 2.0) * 1);
+	map->scene.rayvec.y = (double)(y - (map->scene.h / 2.0) * 1);
+	map->scene.rayvec.z = -(map->scene.w / 2 * tan((90.0 / 2.0) * M_PI / 180.0));
+	//normalize_vec(&(map->scene.rayvec));
+	tmp = get_contact(map, map->scene.rayvec);
 	if (tmp > 0.0)
 		draw_pixel_to_image(map, x, y, 0xFF0000);
 	//printf("x: %f| y: %f\n", rayvec.x, rayvec.y);
