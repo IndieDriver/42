a1565fc084ecaab551fbf3d8772e18e537ecdfd3
IndieDriver <amathias63@gmail.com>
2017-02-19 18:55:44 +0100

    malloc: malloc of tiny and small

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -3,32 +3,60 @@
 /*
 TINY: 1 at 512 bytes (65536 bytes)
 SMALL: 513 at 4096 bytes (409600 bytes)
-*/
+ */
 void init_malloc()
 {
-  size_t tiny_size;
-  size_t small_size;
-
-  tiny_size = 128 * 512;
-  small_size = 100 * 4096;
-  void *tiny = mmap(NULL, tiny_size, PROT_WRITE | PROT_EXEC | PROT_READ,
-                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  void *small = mmap(NULL, small_size, PROT_WRITE | PROT_EXEC | PROT_READ,
-                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  (void)tiny;
-  (void)small;
+    t_chunk tiny;
+    t_chunk small;
+    size_t tiny_size;
+    size_t small_size;
+    ft_putstr("init_malloc\n");
+    tiny_size = BLOCKS_MAX * TINY_MAX;
+    small_size = BLOCKS_MAX * SMALL_MAX;
+    tiny.start = mmap(NULL, tiny_size, PROT_WRITE | PROT_EXEC | PROT_READ,
+            MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    small.start = mmap(NULL, small_size, PROT_WRITE | PROT_EXEC | PROT_READ,
+            MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    ft_memset(tiny.start, 0, sizeof(size_t) * BLOCKS_MAX);
+    ft_memset(small.start, 0, sizeof(size_t) * BLOCKS_MAX);
+    smalloc.tiny = &tiny;
+    smalloc.small = &small;
 }
 
-void *malloc(size_t size)
+void    *malloc_small(t_chunk *chunk, size_t chunk_size, size_t malloc_size){
+    int i;
+
+    i = 0;
+    while (i < BLOCKS_MAX)
+    {
+        ft_putstr("iter\n");
+        if (chunk->blocks[i] == 0)
+        { 
+            ft_putstr("after if\n");
+            chunk->blocks[i] = malloc_size; 
+            ft_putstr("after chunk->blocks[i] = malloc_size\n");
+            return (chunk->start + (i * chunk_size)); 
+        }
+        i++;
+    }
+    return (NULL);
+}
+
+void    *malloc(size_t size)
 {
-    
-    init_malloc();
-    ft_putstr("call malloc\n");
-    void *ptr = mmap(NULL, size,PROT_WRITE | PROT_EXEC | PROT_READ,
-                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (!ptr)
-      ft_putstr("NULL\n");
+    if (smalloc.tiny == NULL)
+        init_malloc();
+    if (size <= 0)
+        return (NULL);
+    else if (size < TINY_MAX)
+        return (malloc_small(smalloc.tiny, TINY_MAX, size));
+    else if (size < SMALL_MAX)
+        return (malloc_small(smalloc.small, SMALL_MAX, size));
     else
-      ft_putstr("NOT NULL\n");
-  return (ptr);
+    {
+        void *ptr = mmap(NULL, size,PROT_WRITE | PROT_EXEC | PROT_READ,
+                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        return (ptr);
+    }
+    return (NULL);
 }
--- malloc/malloc.h
+++ malloc/malloc.h
@@ -9,31 +9,32 @@
 
 # define TINY_MAX 512
 # define SMALL_MAX 4096
+# define BLOCKS_MAX 128
 
-typedef struct s_alloc
+typedef struct  s_alloc
 {
-  void         *ptr;
-  void         *next;
-}              t_alloc;
+    void        *ptr;
+    void        *next;
+}               t_alloc;
 
-typedef struct s_chunk
+typedef struct  s_chunk
 {
-  void         *start;
-  t_alloc      *ptr;
-}              t_chunk;
+    void        *start;
+    int         blocks[BLOCKS_MAX];
+}               t_chunk;
 
-typedef struct s_malloc{
-    t_chunk    *tiny;
-    t_chunk    *small;
-    t_alloc    *large;
-}              t_malloc;
+typedef struct  s_malloc{
+    t_chunk     *tiny;
+    t_chunk     *small;
+    t_alloc     *large;
+}               t_malloc;
 
 
-t_malloc       smalloc;
+t_malloc        smalloc;
 
-void free(void *ptr);
-void *malloc(size_t size);
-void *realloc(void *ptr, size_t size);
-void show_alloc_mem();
+void            free(void *ptr);
+void            *malloc(size_t size);
+void            *realloc(void *ptr, size_t size);
+void            show_alloc_mem();
 
 #endif
