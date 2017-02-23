ea4f9b579539cb92d9ef938374f810cba473e1ed
IndieDriver <amathias63@gmail.com>
2017-02-23 21:13:24 +0100

    malloc: realloc

--- malloc/realloc.c
+++ malloc/realloc.c
@@ -1,5 +1,19 @@
 #include "malloc.h"
 
+void    *copy_chunk(void *ptr, size_t old_size, size_t new_size)
+{
+    void *new_ptr;
+    
+    new_ptr = malloc(new_size);
+    if (new_ptr)
+    {
+        new_ptr = ft_memcpy((void*)new_ptr, (void*)ptr + sizeof(t_chunk), old_size);
+        free(ptr);
+        return (new_ptr);
+    }
+    return (NULL);
+}
+
 void    *realloc_small(t_chunk **chunk, size_t chunk_size, void *ptr, size_t new_size)
 {
     t_chunk *temp;
@@ -11,18 +25,33 @@ void    *realloc_small(t_chunk **chunk, size_t chunk_size, void *ptr, size_t new
         i = 0;
         while (i < BLOCKS_MAX)
         {
-            if (temp + sizeof(t_chunk) + (i * chunk_size) == ptr)
+            if ((void*)temp + sizeof(t_chunk) + (i * chunk_size) == ptr)
             {
                 if (new_size < chunk_size){
                     temp->blocks[i] = new_size;
-                    return (ptr); //Node is already at the good size
+                    return (ptr); 
                 }
-                //TODO: create new node, copy and free 
+                return ((void*)copy_chunk(ptr, temp->blocks[i], new_size));
             }
             i++;
         }
+        temp = temp->next;
     }
     return (NULL);
+}
+
+void    *copy_alloc(void *ptr, size_t old_size, size_t new_size)
+{
+    void *new_ptr;
+    
+    new_ptr = malloc(new_size);
+    if (new_ptr)
+    {
+        new_ptr = ft_memcpy((void*)new_ptr, (void*)ptr + sizeof(t_alloc), old_size);
+        free(ptr); 
+        return (new_ptr);
+    }
+    return (NULL); 
 }
 
 void    *realloc_large(t_alloc **alloc, void *ptr, size_t new_size)
@@ -30,11 +59,10 @@ void    *realloc_large(t_alloc **alloc, void *ptr, size_t new_size)
     t_alloc *temp;
     size_t  old_size;
 
-    (void)new_size;
     temp = *alloc;
     while (temp)
     {
-        if (temp + sizeof(t_alloc) == ptr)
+        if ((void*)temp + sizeof(t_alloc) == ptr)
         {
             old_size = temp->size;
             if (new_size < old_size){
@@ -42,7 +70,7 @@ void    *realloc_large(t_alloc **alloc, void *ptr, size_t new_size)
                 temp->size = new_size;
                 return (ptr);
             }
-            //TODO: if new_size buffer create new node, copy and free previous one
+            return ((void*)copy_alloc(ptr, old_size, new_size)); 
         }
         temp = temp->next;
     }
