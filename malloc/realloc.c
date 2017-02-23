19f43fdbc9f49907038ac16169d7a6f14da3f944
IndieDriver <amathias63@gmail.com>
2017-02-23 07:56:15 +0100

    malloc: realloc on smaller size

--- malloc/realloc.c
+++ malloc/realloc.c
@@ -28,6 +28,7 @@ void    *realloc_small(t_chunk **chunk, size_t chunk_size, void *ptr, size_t new
 void    *realloc_large(t_alloc **alloc, void *ptr, size_t new_size)
 {
     t_alloc *temp;
+    size_t  old_size;
 
     (void)new_size;
     temp = *alloc;
@@ -35,7 +36,13 @@ void    *realloc_large(t_alloc **alloc, void *ptr, size_t new_size)
     {
         if (temp + sizeof(t_alloc) == ptr)
         {
+            old_size = temp->size;
             //TODO: if new_size smaller munmap remaining
+            if (new_size < old_size){
+                munmap(ptr + new_size, old_size); 
+                temp->size = new_size;
+                return (ptr);
+            }
             //TODO: if new_size buffer create new node, copy and free previous one
         }
         temp = temp->next;
@@ -46,7 +53,13 @@ void    *realloc_large(t_alloc **alloc, void *ptr, size_t new_size)
 void	*realloc(void *ptr, size_t size)
 {
     printf("call realloc: %p (size: %ld)\n", ptr, size);
-	(void)ptr;
-	(void)size;
+    void *new_ptr;
+
+    if ((new_ptr = realloc_small(&smalloc.tiny, TINY_MAX, ptr, size)) != NULL)
+        return (new_ptr);
+    if ((new_ptr = realloc_small(&smalloc.small, SMALL_MAX, ptr, size)) != NULL)
+        return (new_ptr);
+    if ((new_ptr = realloc_large(&smalloc.large, ptr, size)) != NULL)
+        return (new_ptr);
 	return (NULL);	
 }
