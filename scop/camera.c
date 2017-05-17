ce2233f92ed203c26d1fdbaa79c985dca1da5ba6
IndieDriver <amathias63@gmail.com>
2017-05-17 09:36:33 +0200

    scop: proj + view matrix fix

--- scop/camera.c
+++ scop/camera.c
@@ -62,18 +62,18 @@ void	get_projmatrix(t_mat4 mat, float fov, float ratio)
 	float	near;
 	float	far;
 	float	frustum;
-	float	oneover;
+	float	tan_half_fov;
 
 	near = 0.1f;
 	far = 100.0f;
 	frustum = near - far;
-	oneover = 1.0f / frustum;
+	tan_half_fov = tanf(fov / 2.0f);
 	get_identity_mat4(mat);
-	mat[5] = 1.0f / tan(fov * (M_PI / 360.0f));
-	mat[0] = (1.0f / tan(fov * (M_PI / 360.0f))) / ratio;
+	mat[0] = 1.0f / (ratio / tan_half_fov);
+	mat[5] = 1.0f / tan_half_fov;
 	mat[10] = (far + near) / frustum;
-	mat[14] = -1.0f;
-	mat[11] = 2.0f * (far * near) / frustum;
+	mat[11] = -1.0f;
+	mat[14] = 2.0f * (far * near) / frustum;
 	mat[15] = 0.0f;
 }
 
@@ -98,7 +98,7 @@ void	get_viewmatrix(t_map *map, t_vec4 pos, t_vec4 center, t_vec4 up)
 	map->viewmat4[2] = -map->cam.dir.x;
 	map->viewmat4[6] = -map->cam.dir.y;
 	map->viewmat4[10] = -map->cam.dir.z;
-	map->viewmat4[3] = -dot(map->cam.right, pos);
-	map->viewmat4[7] = -dot(u, pos);
-	map->viewmat4[11] = -dot(map->cam.dir, pos);
+	map->viewmat4[12] = -dot(map->cam.right, pos);
+	map->viewmat4[13] = -dot(u, pos);
+	map->viewmat4[14] = dot(map->cam.dir, pos);
 }
