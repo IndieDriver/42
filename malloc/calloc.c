b1573e6dd608576605d86b562569210dd4a7c3d3
IndieDriver <amathias63@gmail.com>
2017-07-09 14:28:01 +0200

    malloc: calloc implementation

---
+++ malloc/calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calloc.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amathias <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/11 12:16:19 by amathias          #+#    #+#             */
+/*   Updated: 2017/03/11 13:12:47 by amathias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "malloc.h"
+
+void *calloc(size_t nitems, size_t size)
+{
+	void *ptr;
+
+	ptr = malloc(nitems * size);
+	if (ptr != NULL)
+		ft_bzero(ptr, nitems * size);
+	return (ptr);
+}
