b838300e1858cadaad63b6792032334d6845e3f1
IndieDriver <amathias63@gmail.com>
2017-10-04 23:48:39 +0200

    ocaml ex01

--- OCAML/d02/ex01/crossover.ml
+++ OCAML/d02/ex01/crossover.ml
@@ -1,16 +1,28 @@
 let crossover lst1 lst2 =
-    let rec loop lst1 lst2 acclst = match lst1 with
-        | [] -> []
-        | head :: tail -> if head = tail then
-            begin
-                let rec loop2 el lst2 = match lst2 with
-                    | [] -> []
-                    | head :: tail ->
-                            if head == el then head
-                            else loop2 (el head ::)
-
-                            loop (count+1) acc t
-                            else loop 0 ((count+1,head) :: acc) t
-            end
+  let rec loop lst1 lst2 acclst = match lst1 with
+    | [] -> []
+    | head :: tail -> if head = tail then
+      begin
+        (*
+        let rec loop2 el lst2 = match lst2 with
+          | [] -> []
+          | head :: tail -> if head == el then head
+            else loop2 (el tail)
+        in
+        loop2 head lst2 *)
+      end
     in
-    loop lst1 list2 []
+    loop lst1 lst2 []
+
+let main () =
+    let rec print_list lst =
+        match lst with
+        | [] -> print_char '\n'
+        | e::l -> print_char e ; print_string ";" ; print_list l
+    in
+    let lst1 = ['a';'a';'a';'b';'b';'b'] in
+    let lst1 = ['a';'b';'a';'b';'b';'b'] in
+    print_list (crossover lst1 lst2);
+    print_string ""
+
+let () = main ()
